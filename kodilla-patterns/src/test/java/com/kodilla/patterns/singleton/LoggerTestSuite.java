b5216757ba5835253edb773fb3327f0532de1e92
Aleks0491 <aleksander.sotor@gmail.com>
2020-02-03 23:17:18 +0100

    Zad 15.1 - popr

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -7,29 +7,22 @@ import org.junit.Test;
 public class LoggerTestSuite {
 //    private static  Logger logger;
 //
-//    @BeforeClass
-//    public static void logging() {
-//        logger = new Logger();
-//        logger.log("First logging");
-//    }
-//
 //    @Test
 //    public void testLogger() {
 //        //Given
+//    logger = new Logger();
+//        logger.log("First logging");
 //        //When
 //        String logName = logger.getLastLog();
 //        System.out.println("Log: " + logName);
 //        //Then
 //        Assert.assertEquals("First logging", logName);
 //    }
-    @BeforeClass
-    public static void logging() {
-        Logger.getInstance().log("First logging");
-    }
 
     @Test
     public void testLogger() {
         //Given
+        Logger.getInstance().log("First logging");
         //When
         String logName = Logger.getInstance().getLastLog();
         System.out.println("Log: " + logName);
