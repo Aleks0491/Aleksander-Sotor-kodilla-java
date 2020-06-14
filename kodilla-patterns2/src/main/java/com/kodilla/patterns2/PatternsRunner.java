88d9113933186bd88c241725edc0c0dba0099fc0
Aleks0491 <aleksander.sotor@gmail.com>
2020-06-14 16:17:35 +0200

    Observable - theory

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/PatternsRunner.java
@@ -0,0 +1,11 @@
+package com.kodilla.patterns2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternsRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(PatternsRunner.class, args);
+    }
+}
